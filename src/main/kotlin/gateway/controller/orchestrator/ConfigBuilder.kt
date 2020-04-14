7fe3ac29da06830db98b3341c3eb927f160e5a94
friendlyanon <friendlyanon@fake.gitlab.com>
2020-04-14 22:29:58 +0200

    Refactor to using a SQL procedure

--- src/main/kotlin/gateway/controller/orchestrator/ConfigBuilder.kt
+++ src/main/kotlin/gateway/controller/orchestrator/ConfigBuilder.kt
@@ -2,122 +2,68 @@ package gateway.controller.orchestrator
 
 import gateway.controller.utils.JSONObject
 import java.sql.Connection
+import java.sql.ResultSet
 import java.sql.Statement
 
-// TODO look into implementing this in a database procedure
 class ConfigBuilder(gatewayId: Int, conn: Connection) {
     val result = JSONObject()
+    private val tables = arrayOf(
+        "stations",
+        "input_parameters",
+        "sensors",
+        "validations",
+        "descriptors",
+        "groups",
+        "sensor_types",
+        "units"
+    ).iterator()
 
     init {
-        result["gateway"] = conn.where("gateways", "id", gatewayId).get()!!
+        val stmt = conn.prepareStatement("CALL `fetch_gateway`(?)")
+        stmt.setInt(1, gatewayId)
 
-        val stations = conn.where("stations", "gateway_id", gatewayId).toList()
-        val stationIds = ids()
-        val inputParameterIds = ids()
-        result["stations"] = stations.toObject(
-            { stationIds.add(it) },
-            { inputParameterIds.add(get("input_parameter_id") as Int) }
-        )
-
-        result["input_parameters"] = conn
-            .where("input_parameters", "id", inputParameterIds)
-            .toList()
-            .toObject()
-
-        val sensors = conn.where("sensors", "station_id", stationIds).toList()
-        val descriptorIds = ids()
-        val sensorTypeIds = ids()
-        val validationIds = ids()
-        result["sensors"] = sensors.toObject(
-            { descriptorIds.add(get("descriptor_id") as Int) },
-            { sensorTypeIds.add(get("sensor_type_ids") as Int) },
-            { get("validation_id")?.let { validationIds.add(it as Int) } }
-        )
-
-        result["validations"] = when {
-            validationIds.isEmpty() -> JSONObject()
-            else -> conn
-                .where("validations", "id", validationIds)
-                .toList()
-                .toObject()
+        var first = true
+        var isResultSet = stmt.execute()
+        while (true) {
+            if (isResultSet) {
+                if (first) {
+                    processGateway(stmt)
+                    first = false
+                } else {
+                    processTable(stmt)
+                }
+            } else if (stmt.updateCount == -1) {
+                break
+            }
+            isResultSet = stmt.getMoreResults(Statement.CLOSE_CURRENT_RESULT)
         }
-
-        val descriptors = conn
-            .where("descriptors", "id", descriptorIds)
-            .toList()
-        val groupIds = ids()
-        val unitSet = mutableSetOf<Int>()
-        result["descriptors"] = descriptors.toObject(
-            { get("group_id")?.let { groupIds.add(it as Int) } },
-            { unitSet.add(get("unit_id") as Int) }
-        )
-
-        result["groups"] = when {
-            groupIds.isEmpty() -> JSONObject()
-            else -> conn.where("groups", "id", groupIds).toList().toObject()
-        }
-
-        result["sensor_types"] = conn
-            .where("sensor_types", "id", sensorTypeIds)
-            .toList()
-            .toObject({ unitSet.add(get("unit_id") as Int) })
-
-        result["units"] = conn
-            .where("units", "id", unitSet.toList())
-            .toList()
-            .toObject()
     }
 
-    private fun ids() = mutableListOf<Int>()
+    private fun processGateway(stmt: Statement) {
+        result["gateway"] = stmt.resultSet.get()!!
+    }
 
-    private fun List<JSONObject>.toObject(
-        vararg transformers: JSONObject.(Int) -> Unit
-    ) = JSONObject().also {
+    private fun processTable(stmt: Statement) {
+        result[tables.next()] = stmt.resultSet.asSequence().toObject()
+    }
+
+    private fun Sequence<JSONObject>.toObject() = JSONObject().also {
         for (obj in this) {
-            val id = obj["id"] as Int
-            it[id.toString()] = obj
-            for (transform in transformers) {
-                it.transform(id)
-            }
+            it[(obj["id"] as Int).toString()] = obj
         }
     }
 
-    private fun Connection.where(tbl: String, column: String, id: Int) =
-        run("SELECT * FROM $tbl WHERE $column = ?", listOf(id))
-
-    private fun Connection.where(tbl: String, column: String, ids: List<Int>) =
-        run("SELECT * FROM $tbl WHERE $column IN ".appendArray(ids), ids)
-
-    private fun String.appendArray(list: List<Int>) = this + when (list.size) {
-        0 -> throw IllegalArgumentException("Empty list")
-        1 -> "(?)"
-        else -> StringBuffer(list.size * 3).apply {
-            append("(?")
-            for (j in 2..list.size) {
-                append(", ?")
-            }
-            append(')')
-        }.toString()
-    }
-
-    private fun Connection.run(sql: String, values: List<Int>): Statement {
-        val stmt = prepareStatement(sql)
-        values.forEachIndexed { i, v -> stmt.setInt(i + 1, v) }
-        return stmt.apply { execute() }
-    }
-
-    private fun Statement.get(): JSONObject? {
-        if (!resultSet.next()) {
+    private fun ResultSet.get(): JSONObject? {
+        if (!next()) {
             return null
         }
 
         val obj = JSONObject()
-        val meta = resultSet.metaData
-        for (i in 1..meta.columnCount) {
-            obj[meta.getColumnName(i)] = resultSet.getObject(i)
+        for (i in 1..metaData.columnCount) {
+            obj[metaData.getColumnName(i)] = getObject(i)
         }
         return obj
     }
 
-    private fun Statement.toList() = generateSequence { get() }.toList()
+    private fun ResultSet.asSequence() = generateSequence { get() }
 }
